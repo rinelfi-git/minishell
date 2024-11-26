11903a4b63bdad9a3df07675c4af629a8b228a02
Rijaniaina Elie Fidèle <elierijaniaina@gmail.com>
2024-11-26 09:25:20 +0300

    configuration de base

--- include/minishell.h
+++ include/minishell.h
@@ -6,11 +6,26 @@
 /*   By: erijania <erijania@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 18:53:21 by erijania          #+#    #+#             */
-/*   Updated: 2024/11/14 18:53:46 by erijania         ###   ########.fr       */
+/*   Updated: 2024/11/26 09:24:59 by erijania         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+typedef struct s_minishell	t_mini;
+typedef struct s_env		t_env;
+
+struct s_minishell
+{
+	int		ret;
+	t_env	*env;
+};
+
+struct s_env
+{
+	char	*name;
+	char	*value;
+	t_env	*next;
+};
 #endif
