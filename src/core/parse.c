66c50a2cddee794e210636ddabd85baf827d1150
erijania <erijania@student.42antananarivo.mg>
2024-12-04 12:51:46 +0300

    style: directive pour le parsing

--- src/core/parse.c
+++ src/core/parse.c
@@ -6,14 +6,19 @@
 /*   By: erijania <erijania@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 20:53:38 by erijania          #+#    #+#             */
-/*   Updated: 2024/12/04 12:46:09 by erijania         ###   ########.fr       */
+/*   Updated: 2024/12/04 12:50:08 by erijania         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	parse(char **str)
+int	parse(t_mini *mini, char **str)
 {
-	
+	// existence double/simple quote
+	// si simple quote ne pas expander les variables qui se trouvent dans la chaine
+	// ex: une chaine qui contient 'une $HOME' => une chaine qui contient une $HOME
+	// si double quote expander les variables qui se trouvent dans la chaine
+	// ex: une chaine qui contient "une $HOME" et exit "$?" => une chaine qui contient une /home/user et exit 0
+	// l'expansion utilise la fonction expand(t_mini, char **)
 	return (1);
 }
