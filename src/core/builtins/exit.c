15a5f78df32d6aa58140425b4e2fa0b35be187fb
erijania <erijania@student.42antananarivo.mg>
2024-12-11 13:09:54 +0300

    refactor: simplifier la fonction dans exit

--- src/core/builtins/exit.c
+++ src/core/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: erijania <erijania@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 10:12:34 by erijania          #+#    #+#             */
-/*   Updated: 2024/12/11 12:54:06 by erijania         ###   ########.fr       */
+/*   Updated: 2024/12/11 13:09:16 by erijania         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,26 @@
 #include "libft.h"
 #include "msutils.h"
 
+static void	exit_not_numeric(t_mini *mini)
+{
+	ft_putendl_fd("minishell: exit: numeric argument required", STDERR_FILENO);
+	mini->exit_code = 2;
+}
+
+static void	exit_numeric(t_mini *mini, int exit_code)
+{
+	data_free(mini);
+	exit(exit_code);
+}
+
+static void	exit_many_argument(t_mini *mini)
+{
+	ft_putendl_fd("minishell: exit: too many arguments", STDERR_FILENO);
+	mini->exit_code = 1;
+}
+
 int	built_exit(t_mini *mini, char **args)
 {
-	int	cast;
 	int	len;
 
 	len = strarraylen(args);
@@ -24,24 +41,13 @@ int	built_exit(t_mini *mini, char **args)
 	if (len == 2)
 	{
 		if (!isnumber(args[1]))
-		{
-			ft_putendl_fd("minishell: exit: numeric argument required", STDERR_FILENO);
-			cast = 2;
-		}
+			exit_not_numeric(mini);
 		else
-			cast = ft_atoi(args[1]);
-		data_free(mini);
-		exit(cast);
+			exit_numeric(mini, ft_atoi(args[1]));
 	}
 	else if(len == 1)
-	{
-		data_free(mini);
-		exit(0);
-	}
+		exit_numeric(mini, 0);
 	else
-	{
-		ft_putendl_fd("minishell: exit: too many arguments", STDERR_FILENO);
-		exit(1);
-	}
-	return (0);
+		exit_many_argument(mini);
+	return (1);
 }
