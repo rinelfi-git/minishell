506c55c6d6d36e250e8c0722b82114d8ecf80814
erijania <erijania@student.42antananarivo.mg>
2024-12-14 15:37:54 +0300

    norm: pwd.c

--- src/core/builtins/pwd.c
+++ src/core/builtins/pwd.c
@@ -6,17 +6,17 @@
 /*   By: erijania <erijania@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 07:33:07 by erijania          #+#    #+#             */
-/*   Updated: 2024/12/11 13:50:49 by erijania         ###   ########.fr       */
+/*   Updated: 2024/12/14 15:37:45 by erijania         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "msutils.h"
 
-int	    built_pwd(t_mini *mini, char **args)
+int	built_pwd(t_mini *mini, char **args)
 {
 	char	*cwd;
-	
+
 	(void)args;
 	cwd = getcwd(0, 0);
 	printf("%s\n", cwd);
